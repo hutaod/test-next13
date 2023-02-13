7dccd4c3986ffa36536efeb4c91bfc037eae4c7d
hutao <1131589588@qq.com>
2023-02-14 07:34:22 +0800

    feat: demo3 update

--- src/pages/isr/demo3/[id].tsx
+++ src/pages/isr/demo3/[id].tsx
@@ -18,7 +18,7 @@ const Demo3 = ({ name, content }: Props) => {
 
 export async function getStaticProps(ctx: GetStaticPropsContext) {
   // 获取详情
-  const res = await fetch(`http://localhost:3000/api/posts/detail?id=${ctx.params?.id}`);
+  const res = await fetch(`${process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://test-next13-alpha.vercel.app"}/api/posts/detail?id=${ctx.params?.id}`);
   const detail = await res.json()
   return {
     props: {
@@ -28,16 +28,16 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
 }
 
 export async function getStaticPaths() {
-  // 从接口获取文章列表
-  const res = await fetch('http://localhost:3000/api/posts/list')
-  const posts = await res.json()
+  // // 从接口获取文章列表
+  // const res = await fetch('http://localhost:3000/api/posts/list')
+  // const posts = await res.json()
 
-  // 处理成 getStaticPaths 需要的返回参数
-  const paths = posts.map((post: any) => ({
-    params: { id: post.id },
-  }))
+  // // 处理成 getStaticPaths 需要的返回参数
+  // const paths = posts.map((post: any) => ({
+  //   params: { id: post.id },
+  // }))
 
-  return { paths, fallback: 'blocking' }
+  return { paths: [], fallback: 'blocking' }
 }
 
 export default Demo3
