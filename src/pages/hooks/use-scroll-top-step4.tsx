4a698d235c99425015709791cbb313c22f25f602
hellott <1131589588@qq.com>
2023-02-16 02:46:02 +0000

    Update use-scroll-top-step4.tsx and use-scroll-top.tsx

--- src/pages/hooks/use-scroll-top-step4.tsx
+++ src/pages/hooks/use-scroll-top-step4.tsx
@@ -1,12 +1,13 @@
 import { useScrollTop } from "@/components/hooks/use-scroll-top-step4";
 import { useMemo } from "react";
 
+const params = {
+  scrollThreshold: 0,
+  minCriticalvalue: 500,
+  maxCriticalvalue: 500,
+};
 function TestUseScrollTop() {
-  const { scrollTop, scrolling } = useScrollTop({
-    scrollThreshold: 0,
-    minCriticalvalue: 500,
-    maxCriticalvalue: 500,
-  });
+  const { scrollTop, scrolling } = useScrollTop(params);
   console.log(`scrollTop:${scrollTop}, scrolling:${scrolling}`);
   return (
     <div style={useMemo(() => ({ height: 10000 }), [])}>
@@ -14,6 +15,7 @@ function TestUseScrollTop() {
         style={useMemo(() => ({ position: "fixed", top: 20, left: 20 }), [])}
       >
         <h3>滚动过500像素触发一次</h3>
+        <p>参数：{JSON.stringify(params, null, 4)}</p>
         <p>scrollTop: {scrollTop}</p>
         <p>scrolling: {scrolling ? "scrolling" : "end"}</p>
       </div>
