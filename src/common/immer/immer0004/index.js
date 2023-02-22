87050ab652bd4a51f7dc746c4f01f32c46fb66fb
hutao <1131589588@qq.com>
2023-02-22 08:19:42 +0800

    test: immer-simple

--- src/common/immer/immer0004/index.js
+++ src/common/immer/immer0004/index.js
@@ -1,6 +1,8 @@
 function immer(baseState, thunk) {
   // 用于缓存被修改的对象，key 为原对象，value 为修改后的对象
   const copies = new Map();
+  // 用于存储被代理的对象，被代理的不一定被修改，被修改的一定被代理。
+  const proxies = new Map();
 
   const handler = {
     get(target, prop) {
@@ -8,12 +10,14 @@ function immer(baseState, thunk) {
       return createProxy(target[prop])
     },
     set(target, prop, value) {
-      const current = target[prop];
-      // 值不等的时候才进行设置值
-      if (current !== value) {
-        const copy = getOrCreateCopy(target);
-        copy[prop] = value; // 给拷贝对象设置值
-      }
+      const copy = getOrCreateCopy(target);
+      copy[prop] = value; // 给拷贝对象设置值
+      // const current = target[prop];
+      // // 值不等的时候才进行设置值
+      // if (current !== value) {
+      //   const copy = getOrCreateCopy(target);
+      //   copy[prop] = value; // 给拷贝对象设置值
+      // }
       return true;
     }
   }
@@ -27,32 +31,60 @@ function immer(baseState, thunk) {
     return copy;
   }
 
-  function createProxy(base) {
+  // function log(cb) {
+  //   return (...args) => {
+  //     const result = cb(...args);
+  //     console.log("args", args, "result", result);
+  //     return result;
+  //   }
+  // }
+
+  const createProxy = function createProxy(base) {
     // 对象和数组进行劫持
     if (isPlainObject(base) || Array.isArray(base)) {
-      const copy = copies.get(base);
+      let proxy = proxies.get(base);
       // 避免重复设置 Proxy 代理
-      if (copy) {
-        return copy;
+      if (!proxy) {
+        proxy = new Proxy(base, handler);
+        proxies.set(base, proxy);
       }
-      return new Proxy(base, handler);
+      return proxy;
     }
     return base
   }
 
-  function finalize(state) {
-    const copy = copies.get(state);
-    const result = copy || { ...state };
-    Object.keys(state).forEach(prop => {
-      // 属性对象有缓存则获取缓存
-      const copy = copies.get(state[prop]);
-      if (copy) { // 如果有 copy 表示被修改过
-        result[prop] = copy // 就是用修改后的内容
-      }
-    })
-    return result;
+  // checks if the given base object has modifications, either because it is modified, or
+  // because one of it's children is
+  function hasChanges(base) {
+    const proxy = proxies.get(base)
+    if (!proxy) return false // nobody did read this object
+    if (copies.has(base)) return true // a copy was created, so there are changes
+    // look deeper
+    const keys = Object.keys(base)
+    for (let i = 0; i < keys.length; i++) {
+        const value = base[keys[i]]
+        if ((Array.isArray(value) || isPlainObject(value)) && hasChanges(value)) return true
+    }
+    return false
   }
-  const proxy = new Proxy(baseState, handler);
+
+  function finalize(state) {
+    if (isPlainObject(state) || Array.isArray(state)) {
+      // 未改变直接返回
+      if(!hasChanges(state)) {
+        return state
+      }
+      // 改变的获取副本进行遍历检查
+      const copy = getOrCreateCopy(state)
+      Object.keys(copy).forEach(prop => {
+        copy[prop] = finalize(state[prop])
+      })
+      return copy;
+    }
+    return state;
+  }
+
+  const proxy = createProxy(baseState);
   thunk(proxy);
   return finalize(baseState)
 }
--- src/pages/immutable/immer0004.tsx
+++ src/pages/immutable/immer0004.tsx
@@ -3,13 +3,15 @@ import immer from "../../common/immer/immer0004";
 
 function TestImmer() {
   useEffect(() => {
-    const state = { label: "HTML", info: { desc: "超文本标记语言" }, info2: { desc: "超文本标记语言" } };
-    const state1 = immer(state, (draft: any) => {
+    const state = { label: "HTML", info: { desc: "超文本标记语言", detail: { h1: { name: "标题标签" } } }, info2: { desc: "超文本标记语言" } };
+    const state1 = immer(state, (draft: typeof state) => {
         draft.label = "HTML5";
         draft.info.desc = "H5";
-    })
-    console.log(state, state1) // false
+        draft.info.detail.h1.name = "H1";
+    }) as typeof state;
+    console.log(state === state1) // false
     console.log(state.info === state1.info) // false
+    console.log(state.info.detail === state1.info.detail) // false
     console.log(state.info2 === state1.info2) // true
   }, [])
 
