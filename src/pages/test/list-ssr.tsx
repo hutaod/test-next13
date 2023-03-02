11646f68e33b3680d4076d9d8c259e63cc0c2d14
hellott <1131589588@qq.com>
2023-03-02 13:08:05 +0000

    Update list-ssg.tsx and list-ssr.tsx

--- src/pages/test/list-ssr.tsx
+++ src/pages/test/list-ssr.tsx
@@ -1,14 +1,14 @@
-import styles from './list.module.css';
+import styles from "./list.module.css";
 import axios from "axios";
 
-const apiBase = "https://cnodejs.org/api/v1"
+const apiBase = "https://cnodejs.org/api/v1";
 
 async function fetcher(apiPath: string) {
-  const res = await axios.get(`${apiBase}${apiPath}`)
+  const res = await axios.get(`${apiBase}${apiPath}`);
   return res.data?.data || [];
 }
 
-function App({ data, errorMsg }: { data: any, errorMsg: string }) {
+function App({ data, errorMsg }: { data: any; errorMsg: string }) {
   return (
     <div className={styles.container}>
       <h2>测试Next SSR渲染</h2>
@@ -16,7 +16,13 @@ function App({ data, errorMsg }: { data: any, errorMsg: string }) {
       <ul>
         {data?.map((item: any) => (
           <li key={item.id}>
-            <a href={`https://cnodejs.org/topic/${item.id}`} target="_blank" rel="noreferrer" >{item.title}</a>
+            <a
+              href={`https://cnodejs.org/topic/${item.id}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {item.title}
+            </a>
           </li>
         ))}
       </ul>
@@ -24,13 +30,12 @@ function App({ data, errorMsg }: { data: any, errorMsg: string }) {
   );
 }
 
-
 export async function getServerSideProps() {
   let data = [];
   let errorMsg = null;
   const timeStart = Date.now();
   try {
-    data = await fetcher("/topics")
+    data = await fetcher("/topics");
   } catch (err) {
     errorMsg = `接口请求异常:${JSON.stringify(err)}`;
   }
@@ -38,10 +43,9 @@ export async function getServerSideProps() {
     props: {
       data,
       errorMsg,
-      apiTime: Date.now() - timeStart,
-    }
-  }
+      apiDurtion: Date.now() - timeStart,
+    },
+  };
 }
-
 
 export default App;
