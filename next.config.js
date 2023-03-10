c6b9b462fd81c5bc45a43259ad539dace5a5bc1b
hutao <1131589588@qq.com>
2023-03-10 22:06:30 +0800

    feat: 删除无用代码

--- next.config.js
+++ next.config.js
@@ -1,27 +1,23 @@
-// const withLess = require("./next-with-less.js");
-const withAntdLess = require('next-plugin-antd-less');  
+const withLess = require("./next-with-less.js");
+const path = require('node:path');
 
-// const path = require('node:path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,  
-  // less配置  
-  ...withAntdLess({})  
-  // webpack(config, options) {
-  //   // disable css-module in Next.js
-  //   config.module.rules.forEach((rule) => {
-  //     const { oneOf } = rule;
-  //     if (oneOf) {
-  //       oneOf.forEach((one) => {
-  //         if (!`${one.issuer?.and}`.includes('_app')) return;
-  //         one.issuer.and = [path.resolve(__dirname)];
-  //       });
-  //     }
-  //   })
+  webpack(config) {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes('_app')) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    })
 
-  //   return config;
-  // }
+    return config;
+  }
 }
 
-module.exports = nextConfig
+module.exports = withLess(nextConfig)
