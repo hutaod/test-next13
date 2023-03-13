24debff6f3632fc97a0f2e24975dc7391c093dda
hutao <1131589588@qq.com>
2023-03-13 21:34:24 +0800

    feat: test nextra docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
 }
 
-module.exports = nextConfig
+module.exports = withNextra(nextConfig)
