ab5ff115096dccad45da189c069142818808e0c7
hutao <1131589588@qq.com>
2023-04-19 22:54:09 +0800

    test metadata

--- app/features/metadata/page.tsx
+++ app/features/metadata/page.tsx
@@ -2,6 +2,7 @@
 //   title: 'test metadata',
 // };
 import Link from "next/link";
+import Script from 'next/script'
 import "../global.css";
 
 import { cache } from "react";
@@ -21,24 +22,26 @@ const getPageInfo = cache(async function getPageInfo() {
 })
 
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const data = await getPageInfo();
-  log("generateMetadata", data)
-  return {
-    title: data.title,
-  };
-}
+// export async function generateMetadata(props: any, ...rest: any) {
+//   console.log(1111, props, rest)
+//   const data = await getPageInfo();
+//   log("generateMetadata", data)
+//   return {
+//     title: data.title,
+//   };
+// }
 
 async function TestMetadata() {
   const data = await getPageInfo();
   log(data)
   return (
     <div>
+      <title>{data?.title}</title>
       <h3>Test Metadata</h3>
+      <meta name='description' content={data?.desc || "hello next.js"} />
+      <link rel='stylesheet' href="https://cdn.bootcdn.net/ajax/libs/animate.css/4.1.1/animate.min.css"></link>
+      {/* <base target="_blank" /> */}
+      <Script src="https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.21/lodash.core.js" />
       <div>{data?.title}</div>
       <Link href={"/features/data-fetch"}>data fetch page</Link>
     </div>
